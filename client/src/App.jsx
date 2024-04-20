7fd2db6dfa52b53c7e0a95cb6f6d5efacd33ee9f
nguyenbathao <thaofamily8900nh@gmail.com>
2024-04-21 02:57:13 +0700

    register and login from front to back

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import PageNotFound from './pages/PageNotFound';
+import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/User/Dashboard';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import {registerController} from '../controllers/authController.js'
+import {registerController, loginController, testController} from '../controllers/authController.js'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
 
 router.post('/register', registerController)
 
+router.post('/login', loginController)
+
+router.get('/test', requireSignIn, isAdmin, testController)
 
 export default router
